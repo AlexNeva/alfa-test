d9c7ad0f716f73af35a010d928e654972419f05e
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-04-02 14:37:50 +0300

    create Photo  service

---
+++ src/API/photoService.ts
@@ -0,0 +1,21 @@
+import { IPhotos } from './../types';
+
+export default class PhotoService {
+
+  API_KEY: string = '17766632-9696463ed60dcf33340f3d2a4';
+  BASE_URL: string = `https://pixabay.com/api/${this.API_KEY}`;
+
+  async getResource<T>(url: string): Promise<T> {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw data
+    }
+    return data;
+  }
+
+  getPhotos(): Promise<IPhotos> {
+    return this.getResource(this.BASE_URL);
+  }
+}
